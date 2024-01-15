2d7d4a1c5ee68f226ddd711089691d1e54c71bb5
betest <yohan.tobon@hotmail.com>
2024-01-14 20:32:13 -0500

    fix commands deploy github pages, astro config (github pages)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://betest.github.io",
   base: "yohan-tobon-dev",
   integrations: [tailwind()]
 });
