ee4f1a81a3f61f879876d38be97bbbf2ee91358b
betest <yohan.tobon@hotmail.com>
2024-01-14 20:44:16 -0500

    fix astro config (deploy error styles)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://betest.github.io",
-  base: "yohan-tobon-dev",
+  base: "/yohan-tobon-dev",
   integrations: [tailwind()]
 });
